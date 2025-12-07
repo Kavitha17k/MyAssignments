06900398a76432e1feee44b2e97586b3d1174372
Kavitha17k <kavithakaran17@gmail.com>
2025-12-07 17:51:35 +0900

    Add files via upload
    Day1 Assignments

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name="John Doe";
+		int age=30;
+		double salary=55000.75;
+		char gender='M';
+		boolean acceptTerms=true;
+		float rating=4.5f;
+		long mobileNumber=9876543210l;
+		int pinCode = 56001;
+		int luckyNumber = 7;
+		
+		System.out.println(name+"\n" +age+"\n" +salary+"\n" +gender+"\n" +acceptTerms+"\n" +rating+"\n" +mobileNumber+"\n" +pinCode+"\n" +luckyNumber+"\n");
+		
+	}
+
+}
---
+++ day1/FirstProg.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+
+public class FirstProg {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("All the best");
+		
+		
+	}
+
+}
